59a991d24c0ef3fe5db35d8b4d321b5eb0735f99
HmaCraftYT <45132766+HmaCraftYT@users.noreply.github.com>
2018-11-21 00:36:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,7 +55,7 @@ client2.on('message', message => {
     if(message.content === '-اعطاء''){
         message.channel.send('#credits <@${message.author.id}> 10000')
     }
-});
+})
 
 client.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
 if (message.content === '!spam') {
